86592e1b9b6232d39b09934bfb5bbfe06ff9c4be
damian <damian.gorski1@wp.pl>
2023-11-05 19:49:25 +0100

    added database for order-service

--- mongo-init.sh
+++ mongo-init.sh
@@ -13,4 +13,18 @@ db.createUser(
     ]
   }
 );
+
+use order
+db.createUser(
+  {
+    user: "order-service",
+    pwd: "Password123!",
+    roles: [
+      {
+        role: "readWrite",
+        db: "order"
+      }
+    ]
+  }
+);
 EOF
